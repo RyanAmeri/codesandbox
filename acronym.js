0924bf77011f94d39ebe3fdeb5145fa9eb0f8896
Ryan Ameri <64014419+RyanAmeri@users.noreply.github.com>
2020-08-06 18:31:35 +1000

    Initial commit

---
+++ acronym.js
@@ -0,0 +1,12 @@
+export const parse = input => {
+  //This Regex matches anything that's not a character or whitespace
+  const regex = /[^a-z\s]+/gi;
+  return input
+    .replace("-", " ") /* first replace '-' with space, */
+    .replace(regex, "") /* remove everything that matches regex */
+    .split(" ") /* turn string to array */
+    .map((word /* return the fist character as uppercase */) =>
+      word.slice(0, 1).toUpperCase()
+    )
+    .join(""); /* Convert array to string and return */
+};
